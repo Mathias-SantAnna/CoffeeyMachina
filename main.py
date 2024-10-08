0384582b808bbde58e54b23c5c71d7e4561a5cc3
Mathias Sant'Anna <dark_tricolor@hotmail.com>
2024-10-09 00:01:15 +0100

    changes in final version of the project

--- main.py
+++ main.py
@@ -1,55 +1,25 @@
+from random import choice
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
-
 from art import logo
 
-
-profit = 0
-
-resources = {
-    "water": 900,
-    "milk": 600,
-    "coffee": 500,
-}
-def is_resource_sufficient(order_ingredients):
-    for item in order_ingredients:
-        if order_ingredients[item] >= resources[item]:
-            print("Sorry there is not enough", item)
-            return False
-    return True
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
 
 is_on = True
 
 while is_on:
     print(logo)
-    print("\nMenu:")
-    for drink in MENU:
-        print(f"{drink.capitalize()}: ${MENU[drink]['cost']}")
-
-    user_request = input(" What would you like to order? Please type: ")
-    if user_request == "off":
+    options = menu.get_items()
+    choice = input(f"\nWhat would you like to order? ({options}): ")
+    if choice == "off":
         is_on = False
-    elif user_request == "report":
-        print(f"\nWater: {resources['water']}ml")
-        print(f"Milk: {resources['milk']}ml")
-        print(f"Coffee: {resources['coffee']}g")
-        print(f"Money: {profit}")
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
     else:
-        drink = MENU[user_request]
-        if is_resource_sufficient(drink["ingredients"]):
-            payment = process_coins()
-            if is_transaction_successful(payment, drink["cost"]):
-                make_coffee(user_request, drink["ingredients"])
-
-"""
-def admin_user():
-    admin_user_request = input(" What would you like to do?: 'report' 'machine maintenance' 'turn machine off'. ")
-    if user_request == "turn machine off":
-        is_on = False
-    elif user_request == "report":
-        print(resources)
-    else:
-        print("machine maintenance started, please wait until is finished.")
-"""
-#TODO: CREATE 2 DIFFERENT USERS: ADMIN AND CLIENT USER
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
